36ae1f1ddee660d23ddc2949a1ebe8e4509e4835
juandjara <juanorigami@gmail.com>
2016-04-03 21:56:58 +0200

    Update 2016-04-03T19:56:57.444Z

--- whereami.js
+++ whereami.js
@@ -1,59 +1,56 @@
-(function(){
 
-  var log_elem = $("#log");
-    
-  function getLocation(){
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(onPositionSuccess, onPositionError);
-    }else{
-      log_elem.text("Geolocation is not supported by this browser.");
-    }
-  }
-
-  window.getLocation = getLocation;
-
-  function onPositionSuccess(position){
-    lat    = position.coords.latitude;
-    lon    = position.coords.longitude;
-    latlon = new google.maps.LatLng(lat, lon);
-    
-    map_elem = $("#map");
-    
-    var options = {
-      center: latlon,
-      zoom: 14,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      mapTypeControl: false,
-      navigationControlOptions: {
-        style: google.maps.NavigationControlStyle.SMALL
-      }
-    };
-    
-    var map    = new google.maps.Map(map_elem, options);
-    var marker = new google.maps.Marker({
-      position: latlon,
-      map: map,
-      title: "You are here!"
-    });
-  }
+var log_elem = $("#log");
   
-  function onPositionError(error){
-    switch(error.code){
-      case error.PERMISSION_DENIED:
-        log_elem.text("User denied the request for Geolocation.");
-        break;
-      case error.POSITION_UNAVAILABLE:
-        log_elem.text("Location information is unavailable.");
-        break;
-      case error.TIMEOUT:
-        log_elem.text("The request to get user location timed out.");
-        break;
-      case error.UNKNOWN_ERROR:
-        log_elem.text("An unknown error occurred.");
-        break;
-    }
+function getLocation(){
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(onPositionSuccess, onPositionError);
+  }else{
+    log_elem.text("Geolocation is not supported by this browser.");
   }
-  
-  getLocation();
+}
 
-})
+window.getLocation = getLocation;
+
+function onPositionSuccess(position){
+  lat    = position.coords.latitude;
+  lon    = position.coords.longitude;
+  latlon = new google.maps.LatLng(lat, lon);
+  
+  map_elem = $("#map");
+  
+  var options = {
+    center: latlon,
+    zoom: 14,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    mapTypeControl: false,
+    navigationControlOptions: {
+      style: google.maps.NavigationControlStyle.SMALL
+    }
+  };
+  
+  var map    = new google.maps.Map(map_elem, options);
+  var marker = new google.maps.Marker({
+    position: latlon,
+    map: map,
+    title: "You are here!"
+  });
+}
+
+function onPositionError(error){
+  switch(error.code){
+    case error.PERMISSION_DENIED:
+      log_elem.text("User denied the request for Geolocation.");
+      break;
+    case error.POSITION_UNAVAILABLE:
+      log_elem.text("Location information is unavailable.");
+      break;
+    case error.TIMEOUT:
+      log_elem.text("The request to get user location timed out.");
+      break;
+    case error.UNKNOWN_ERROR:
+      log_elem.text("An unknown error occurred.");
+      break;
+  }
+}
+
+getLocation();
